e54e50b840ad6ba69fd17ba123019a9442bd1b6c
Indrave <806344324@qq.com>
2017-09-03 18:09:49 +0800

    Initial commit

---
+++ src/com/hfutse/demo/ConcurrencyTest.java
@@ -0,0 +1,54 @@
+package com.hfutse.demo;
+
+/**
+ * Author Indrave
+ * 2017/9/3 17:49
+ */
+public class ConcurrencyTest {
+
+    private static final long count = 100001;
+
+    public static void main(String[] args) throws InterruptedException {
+        concurrency();
+        serial();
+    }
+
+    private static void concurrency() throws InterruptedException {
+        long start = System.currentTimeMillis();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int a = 0;
+                for (int i = 0; i < count; i++) {
+                    a += 5;
+                }
+            }
+        });
+        thread.start();
+        int b = 0;
+        for (int j = 0; j < count; j++) {
+            b--;
+        }
+        thread.join();
+        long time = System.currentTimeMillis() - start;
+        System.out.println("concurrency : " + time + "ms,b=" + b);
+
+    }
+
+    private static void serial() {
+        long start = System.currentTimeMillis();
+        int a = 0;
+        for (int i = 0; i < count; i++) {
+            a += 5;
+        }
+        int b = 0;
+        for (int j = 0; j < count; j++) {
+            b--;
+        }
+        long time = System.currentTimeMillis() - start;
+        System.out.println("serial : " + time + "ms,b=" + b);
+
+    }
+
+
+}
